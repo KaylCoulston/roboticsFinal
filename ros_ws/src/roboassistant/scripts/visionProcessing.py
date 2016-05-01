82d4b71e99323f5e6d6fb9993d445370e76ab206
Kayl <kaylcoulston@gmail.com>
2016-05-01 15:19:42 -0700

    working fully with color trackbars

--- ros_ws/src/roboassistant/scripts/visionProcessing.py
+++ ros_ws/src/roboassistant/scripts/visionProcessing.py
@@ -25,12 +25,13 @@ TARGET_MASK = "Masked Image"
 cv2.namedWindow(TARGET_MASK)
 
 #HSV filter values
-h_min = 69
-h_max = 80
-s_min = 137
-s_max = 232
+#This is for green
+h_min = 48
+h_max = 90
+s_min = 55
+s_max = 148
 v_min = 84
-v_max = 255
+v_max = 156
 
 def nothing(x):
     pass
@@ -41,6 +42,9 @@ cv2.createTrackbar('LowS', TARGET_MASK, s_min, 255, nothing)
 cv2.createTrackbar('HighS', TARGET_MASK, s_max, 255, nothing)
 cv2.createTrackbar('LowV', TARGET_MASK, v_min, 255, nothing)
 cv2.createTrackbar('HighV', TARGET_MASK, v_max, 255, nothing)
+
+erodeKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+dilateKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
 
 class Kinect_Ros ():
     def __init__ (self):
@@ -54,95 +58,118 @@ class Kinect_Ros ():
 
     def image_callback(self, ros_image):
         self.image = np.asarray(ToOpenCV(ros_image))
-        #TODO: Fix these ranges for two colors
-        #self.lower_target_range_one = (0, 170, 0)
-        #self.upper_target_range_one = (200, 255, 255)
-        self.lower_target_range_two =  (0, 170, 0)
-        self.upper_target_range_one = (200, 255, 255)
+
+        #Green
+        self.lower_target_range_one = (48, 55, 84)
+        self.upper_target_range_one = (90, 148, 156)
+
+        #Red
+        self.lower_target_range_two =  (145, 71, 168)
+        self.upper_target_range_two = (179, 156, 214)
+
+        #get trackbar Location
+        h_min = cv2.getTrackbarPos('LowH', TARGET_MASK)
+        h_max = cv2.getTrackbarPos('HighH', TARGET_MASK)
+        s_min = cv2.getTrackbarPos('LowS', TARGET_MASK)
+        s_max = cv2.getTrackbarPos('HighS', TARGET_MASK)
+        v_min = cv2.getTrackbarPos('LowV', TARGET_MASK)
+        v_max = cv2.getTrackbarPos('HighV', TARGET_MASK)
 
         #test
         self.lower_target_range_one = (h_min, s_min, v_min)
         self.upper_target_range_one = (h_max, s_max, v_max)
 
-        process_image(self)
 
-        def saveDepthImage(self, data):
-            depth_image = self.bridge.imgmsg_to_cv(data, '32FC1')
-            self.depth_array = np.array(depth_image, dtype=np.float32)
+        self.process_image()
 
-        def getDepth(self,x,y):
-            try:
-                return self.depth_array[y][x]
-            except:
-                self.logfile.write("Error occured in getDepth...\n")
-                return -1
+    def saveDepthImage(self, data):
+        depth_image = self.bridge.imgmsg_to_cv2(data, '32FC1')
+        self.depth_array = np.array(depth_image, dtype=np.float32)
 
-        def process_image(self):
-            images = dict()
+    def getDepth(self,x,y):
+        try:
+            return self.depth_array[y][x]
+        except:
+            self.logfile.write("Error occured in getDepth...\n")
+            return -1
 
-            images["Src"] = self.image    #deepcopy (image)
-            images["HSV"] = cv2.cvtColor (self.image, cv2.COLOR_BGR2HSV)
+    def process_image(self):
+        images = dict()
 
-            images["OuterColor"] = cv2.inRange (images["HSV"], self.lower_target_range_one, self.upper_target_range_one)
-            images["InnerColor"] = cv2.inRange (images["HSV"], self.lower_target_range_two, self.upper_target_range_two)
+        images["Src"] = self.image    #deepcopy (image)
+        images["HSV"] = cv2.cvtColor (self.image, cv2.COLOR_BGR2HSV)
 
-            #Show images
-            #test
-            cv2.imshow (TARGET_MASK, images["OuterColor"])
-            cv2.waitKey(25)
+        images["OuterColor"] = cv2.inRange (images["HSV"], self.lower_target_range_one, self.upper_target_range_one)
+        images["InnerColor"] = cv2.inRange (images["HSV"], self.lower_target_range_two, self.upper_target_range_two)
 
-            cv2.imshow ("Color One Processed", images["OuterColor"])
-            cv2.imshow ("Color Two Processed", images["InnerColor"])
-            cv2.waitKey(25)
+        #clear up image
+        images["OuterColor"] = cv2.erode(images["OuterColor"], erodeKernel, iterations = 2)
+        images["OuterColor"] = cv2.dilate(images["OuterColor"], dilateKernel, iterations = 1)
 
-            #find targets
-            outer_x, outer_y, outer_w, outer_h = find_target(self, images["OuterColor"])
-            inner_x, inner_y, inner_w, inner_h = find_target(self, images["InnerColor"])
+        #clear up image
+        images["InnerColor"] = cv2.erode(images["OuterColor"], erodeKernel, iterations = 2)
+        images["InnerColor"] = cv2.dilate(images["OuterColor"], dilateKernel, iterations = 1)
 
-            center_inner_x = inner_x + inner_w / 2
-            center_inner_y = inner_y + inner_h / 2
 
-            #Check if center_inner is inside outer square
-            if((center_inner_x > outer_x and center_inner_x < (outer_x + outer_w)) and (center_inner_y > outer_y and center_inner_y < (outer_y + outer_h))):
-                #get distance
-                depth = getDepth(self, center_inner_x, center_inner_y)
 
-                #TODO: Fix
-                #Print content
-                #ROS_INFO("Target: %s" str(center_inner_x) + ", " + str(center_inner_y) + ", " + str(depth))
+        #Show images
+        #test
+        cv2.imshow (TARGET_MASK, images["OuterColor"])
+        cv2.waitKey(25)
 
-                #declare position variable
-                binPosition = BinPosition();
+        cv2.imshow ("Color One Processed", images["OuterColor"])
+        cv2.imshow ("Color Two Processed", images["InnerColor"])
+        cv2.waitKey(25)
 
-                #set data
-                binPosition.x = center_x
-                binPosition.y = center_y
-                binPosition.distance = depth
+        #find targets
+        outer_x, outer_y, outer_w, outer_h = self.find_target(images["OuterColor"])
+        inner_x, inner_y, inner_w, inner_h = self.find_target(images["InnerColor"])
 
-                #publish
-                self.publish.publish(binPosition)
-            else:
-                ROS_INFO("Target NOT found")
+        center_inner_x = inner_x + inner_w / 2
+        center_inner_y = inner_y + inner_h / 2
 
-        def find_target(self, image):
-            # Target Location
-            contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #Check if center_inner is inside outer square
+        if((center_inner_x > outer_x and center_inner_x < (outer_x + outer_w)) and (center_inner_y > outer_y and center_inner_y < (outer_y + outer_h))):
+            #get distance
+            depth = self.getDepth(center_inner_x, center_inner_y)
 
-            # Find the index of the largest contour
-            areas = [cv2.contourArea(c) for c in contours]
+            #TODO: Fix
+            #Print content
+            #ROS_INFO("Target: %s" str(center_inner_x) + ", " + str(center_inner_y) + ", " + str(depth))
 
-            #Initialize for use
-            x = y = w = h = 0
+            #declare position variable
+            binPosition = BinPosition();
 
-            if len(areas) >= 1:
-                max_index = np.argmax(areas)
-                cnt = contours[max_index]
+            #set data
+            binPosition.x = center_inner_x
+            binPosition.y = center_inner_y
+            binPosition.distance = depth
 
-                # Bounding rectangle
-                x, y, w, h = cv2.boundingRect(cnt)
-                cv2.rectangle(images["Src"], (x,y), (x+w, y+h), (0,0,255), 2)
+            #publish
+            self.publish.publish(binPosition)
+        #else:
+            #ROS_INFO("Target NOT found")
 
-                return (x, y, w, h)
+    def find_target(self, image):
+        # Target Location
+        contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+        # Find the index of the largest contour
+        areas = [cv2.contourArea(c) for c in contours]
+
+        #Initialize for use
+        x = y = w = h = 0
+
+        if len(areas) >= 1:
+            max_index = np.argmax(areas)
+            cnt = contours[max_index]
+
+            # Bounding rectangle
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(image, (x,y), (x+w, y+h), (0,0,255), 2)
+
+            return (x, y, w, h)
+        else:
+            return(-1, -1, -1, -1)
 
 kinect = Kinect_Ros()
