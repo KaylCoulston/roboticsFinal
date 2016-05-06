6461b231bf26d770d512195b9a5dff6b2d561987
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2016-05-05 22:19:05 -0700

    find human updated

--- ros_ws/src/roboassistant/scripts/find_human_server.py
+++ ros_ws/src/roboassistant/scripts/find_human_server.py
@@ -25,27 +25,28 @@ def ToOpenCV(ros_image):
     print(e)
     raise Exception("Failed to convert to OpenCV image")
 
-class MoveCloseToTagServer:
+class FindHumanServer:
     # create messages that are used to publish feedback/result
     __feedback = FindHumanFeedback()
     __result = FindHumanResult()
 
     def __init__(self, name):
         self.__action_name = name
-	self.cmd_vel = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=1)
+        self.cmd_vel = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=1)
         rospy.Subscriber('/camera/rgb/image_raw', Image, self.image_callback)
-	rospy.sleep(2)
+        rospy.sleep(2)
 
         self.server = actionlib.SimpleActionServer(self.__action_name, FindHumanAction, execute_cb=self.execute, auto_start=False)
         self.server.start()
 
     def image_callback(self, data):
         self.image = np.asarray(ToOpenCV(data)) #convert image
-        self.height, self.width, _ = image.shape
+        cv2.imshow("test", self.image)
+        cv2.waitKey(1)
 
     def execute(self, goal):
         print "Executing"
-        
+
         person_found = 0
 
         while(1):
@@ -70,12 +71,13 @@ class MoveCloseToTagServer:
                 soundhandle = SoundClient()
                 rospy.sleep(1)
                 soundhandle.playWave('Human please help clear my path')
+                rospy.sleep(2)
             else:
                 # Keep looking for human (spinning)
                 twist_cmd = Twist()
                 rospy.sleep(1)
                 twist_cmd.angular.z = .5
-                self.cmd_vel.publish(twist_cmd)    
+                self.cmd_vel.publish(twist_cmd)
 
 
 
