d7060bde2c29c5c12683247ac7250ad3d803ad40
Kayl <kaylcoulston@gmail.com>
2016-05-05 17:15:47 -0700

    Changing computer, so pushing code

---
+++ ros_ws/src/roboassistant/scripts/find_human_client.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+import roslib; roslib.load_manifest('roboassistant')
+import rospy
+
+# Brings in the SimpleActionClient
+import actionlib
+
+# Brings in the messages used by the FindHuman action, including the
+# goal message and the result message.
+from roboassistant.msg import FindHumanAction, FindHumanGoal
+
+
+class MoveCloseToTag():
+    def __init__(self):
+        self.goal_sent = False
+
+        # What to do if shut down (e.g. Ctrl-C or failure)
+        rospy.on_shutdown(self.shutdown)
+
+        # Creates the SimpleActionClient, passing the type of the action
+        # (FibonacciAction) to the constructor.
+        self.client = actionlib.SimpleActionClient('find_human', MoveCloseToTagAction)
+        rospy.loginfo("Wait for the action server to come up")
+
+        # Waits until the action server has started up and started
+        # listening for goals.
+        self.client.wait_for_server(rospy.Duration(5))
+
+    def move_to(self, ar_id_goal):
+        # Send a goal
+        self.goal_sent = True
+        # Creates a goal to send to the action server.
+        goal = MoveCloseToTagGoal(ar_tag_id=ar_id_goal)
+
+        # Sends the goal to the action server.
+        self.client.send_goal(goal)
+
+        # Waits for the server to finish performing the action.
+        self.client.wait_for_result(rospy.Duration(60))
+
+        # Prints out the result of executing the action
+        self.goal_sent = False
+        return self.client.get_result()  # A FibonacciResult
+
+    def shutdown(self):
+        if self.goal_sent:
+            self.client.cancel_goal()
+        rospy.loginfo("Stop")
+        rospy.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        # Initializes a rospy node so that the SimpleActionClient can
+        # publish and subscribe over ROS.
+        rospy.init_node('find_human_client')
+        navigator = MoveCloseToTag()
+        result = navigator.move_to(7)
+        print "Result:", "Failed!" if result.completed else "Success!"
+    except rospy.ROSInterruptException:
+        print "program interrupted before completion"
